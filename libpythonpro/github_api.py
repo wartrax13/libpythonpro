981bfc35d1f8dae802a15ee354ff3bafd01753e7
Pedro <pedro.moisesandrade@gmail.com>
2021-06-16 09:21:53 -0300

    Virtual env 2

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -1,3 +1,4 @@
+import requests
 def buscar_avatar(usuario):
     """
     Busca o avatar de um usuario de GitHub
@@ -5,8 +6,8 @@ def buscar_avatar(usuario):
     :return: str com o link do avatar
     """
     url= f'https://api.github.com/users/{usuario}'
-    resp = requestes.get(url)
-    resp.json()['avatar_url']
+    resp = requests.get(url)
+    return resp.json()['avatar_url']
 
 if __name__ == '__main__':
     print(buscar_avatar('wartrax13'))
