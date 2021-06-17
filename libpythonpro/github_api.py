a5f0e660b2cec66ef09430757e4585cbec98e9f1
Pedro <pedro.moisesandrade@gmail.com>
2021-06-17 12:32:31 -0300

    Link da foto do Git

---
+++ libpythonpro/github_api.py
@@ -0,0 +1,15 @@
+import requests
+
+def buscar_avatar(usuario):
+    """
+    Buscar o avatar de um usuario no GitHub
+
+    :param usuario: str com o nome de usuario do github
+    :return: str com o link do avatar
+    """
+    url=f'https://api.github.com/users/{usuario}'
+    resp= requests.get(url)
+    return resp.json()['avatar_url']
+
+if __name__ == '__main__':
+    print(buscar_avatar('wartrax13'))
