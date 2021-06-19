784423fc2dbc130943118124d0d03f0818d3b783
Pedro <pedro.moisesandrade@gmail.com>
2021-06-19 15:33:30 -0300

    TDD, Baby Steps, Parametrização de teste e Teste de exceções

---
+++ libpythonpro/tests/test_spam/test_enviador_de_email.py
@@ -0,0 +1,38 @@
+import pytest
+
+from libpythonpro.spam.enviador_de_email import Enviador, EmailInvalido
+
+
+def test_criar_enviador_de_email():
+    enviador= Enviador()
+    assert enviador is not None
+
+@pytest.mark.parametrize(
+    'destinatario',
+    ['foo@bar.com.br', 'pedro.moisesandrade@gmail.com']
+)
+def test_remetente(destinatario):
+    enviador = Enviador()
+    resultado = enviador.enviar(
+        destinatario,
+        'wartrax13@gmail.com',
+        'Cursos Python Pro',
+        'A primeira turma já está aberta. Testando...'
+    )
+    assert destinatario in resultado
+
+
+@pytest.mark.parametrize(
+    'remetente',
+    [' ', 'pedro']
+)
+def test_remetente(remetente):
+    enviador = Enviador()
+    with pytest.raises(EmailInvalido):
+        enviador.enviar(
+            remetente,
+            'wartrax13@gmail.com',
+            'Cursos Python Pro',
+            'A primeira turma já está aberta. Testando...'
+        )
+
