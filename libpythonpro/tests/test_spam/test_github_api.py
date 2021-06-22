149458ee21b89d8559bacd144b8e030fdced326b
Pedro <pedro.moisesandrade@gmail.com>
2021-06-22 15:58:50 -0300

    Test GitHub com mock

--- libpythonpro/tests/test_spam/test_github_api.py
+++ libpythonpro/tests/test_spam/test_github_api.py
@@ -1,14 +1,28 @@
 from unittest.mock import Mock
 
+import pytest
+
 from libpythonpro import github_api
 
 
-def test_buscar_avatar():
+@pytest.fixture
+def avatar_url(mocker):
     resp_mock = Mock()
+    url = 'https://avatars.githubusercontent.com/u/84944254?v=4'
     resp_mock.json.return_value = {
-        'login':'wartrax13','id' : 84944254,
-        'avatar_url': 'https://avatars.githubusercontent.com/u/84944254?v=4',
+        'login': 'wartrax13', 'id': 84944254,
+        'avatar_url': url,
     }
-    github_api.requests.get = Mock(return_value=resp_mock)
+    get_mock = mocker.patch('libpythonpro.github_api.requests.get')
+    get_mock.return_value = resp_mock
+    return url
+
+
+def test_buscar_avatar(avatar_url):
+    url = github_api.buscar_avatar('wartrax13')
+    assert avatar_url == url
+
+
+def test_buscar_avatar_integracao():
     url = github_api.buscar_avatar('wartrax13')
     assert 'https://avatars.githubusercontent.com/u/84944254?v=4' == url
